6a3b85e929a50cf555d0b92a490a620de6fba1c8
Jaykumar Halpati <rameshhalpati007@gmail.com>
2020-09-07 20:42:26 +0100

    Connected API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Searchbar from './Searchbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to AutoComplete-Searchbar</h1>
+      {/* <Searchbar/> */}
     </div>
   );
 }
