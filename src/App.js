9370a7cdff5448d3f827240ceec8e3edad069346
Jaykumar Halpati <rameshhalpati007@gmail.com>
2020-09-19 20:20:38 +0100

    Resolved API issue

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 import axios from "axios";
 import ReactDOM from "react-dom";
 import Image from "react-bootstrap/Image";
-import { Container, Col, Row } from 'react-bootstrap';
-
+import "./App.css";
 
 // const classes = useStyles();
 
@@ -13,55 +12,52 @@ class App extends Component {
   };
 
   componentDidMount() {
-    axios.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=badlynwl0NVQ6og2yffuO1lghbwqS9vxFO13UF9l").then((res) => {
-      console.log(res);
-      this.setState({
-        photos: res.data.photos.slice(0,10), 
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=badlynwl0NVQ6og2yffuO1lghbwqS9vxFO13UF9l"
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          photos: res.data,
+        });
       });
-    });
   }
 
   render() {
     const { photos } = this.state;
-    const photolength = photos.length ? (
-      photos.map((photos) => {
-        return (
-          <div>
-            <p key={photos.id}></p>
-            <p>Rover name:{photos.rover.name}</p>
-            <p>Landing date:{photos.rover.landing_date}</p>
-            <p>Launch date:{photos.rover.launch_date}</p>
-            {/* <img src={photos.url} width="128" height="128"></img> */}
-            {/* <img class="ui medium rounded image" src={photos.url}></img> */}
-            <Container>
-              <Row>
-                <Col xs={6} md={4}>
-                  <Image src={photos.img_src}rounded width="720" height="640" />
-                </Col>
-              </Row>
-            </Container>
-          </div>
-        );
-      })
-    ) : (
-      <p>No photos yet</p>
-    );
 
     return (
-      <div className="container">
-        {photolength}
-        {/* <div>
-          <Card className={classes.root}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image={photos.id}
-                title="Contemplative Reptile"
-              />
-            </CardActionArea>
-          </Card>
-        </div> */}
+      <div className="app">
+        <div className="nasa-photo">
+          
+          {/* <img src={photos.url} className="photo"/> */}
+          {photos.media_type === "image" ? (
+        <img
+          src={photos.url}
+          alt={photos.title}
+          className="photo"
+        />
+      ) : (
+        <iframe
+          title="space-video"
+          src={photos.url}
+          frameBorder="0"
+          gesture="media"
+          allow="encrypted-media"
+          allowFullScreen
+          className="photo"
+        />
+      )}
+        </div>
+        <h1 className="date">{photos.date}</h1>
+        <h1>{photos.title}</h1>
+        <p className="title">{photos.explanation}</p>
+
+        <footer>Created by<a href="https://www.jayhalpati.me" target="_blank"> Jaykumar Halpati</a></footer>
+
       </div>
+
     );
   }
 }
